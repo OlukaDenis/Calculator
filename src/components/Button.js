0f16119103489739d678b3edefba2c1d2123d38f
OlukaDenis <olukadeno@gmail.com>
2020-06-25 02:05:05 +0300

    Adde the styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Button.scss';
 
-export default function Button({ name }) {
+export default function Button({ name, color, wide}) {
+
+  const btnWidth = wide ? '50%' : '25%';
   return (
     <button
       type="button"
       className="buttonSquare"
+      style={{backgroundColor: color, width: btnWidth}}
     >
       {name}
     </button>
@@ -15,8 +18,11 @@ export default function Button({ name }) {
 
 Button.propTypes = {
   name: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   name: '',
+  wide: '25%',
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './Button';
-import '../styles/ButtonPanel.scss';
+import styles from '../styles/ButtonPanel.scss';
 
 export default function ButtonPanel() {
   return (
@@ -9,30 +9,30 @@ export default function ButtonPanel() {
         <Button name="AC" />
         <Button name="+/-" />
         <Button name="%" />
-        <Button name="÷" />
+        <Button color={styles.lightOrange} name="÷" />
       </div>
       <div className="groupTwo">
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
-        <Button name="X" />
+        <Button color={styles.lightOrange} name="X" />
       </div>
       <div className="groupThree">
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
-        <Button name="-" />
+        <Button color={styles.lightOrange} name="-" />
       </div>
       <div className="groupFour">
         <Button name="1" />
         <Button name="2" />
         <Button name="3" />
-        <Button name="+" />
+        <Button color={styles.lightOrange} name="+" />
       </div>
       <div className="groupFive">
-        <Button name="0" />
+        <Button wide={true} name="0" />
         <Button name="." />
-        <Button name="=" />
+        <Button color={styles.lightOrange} name="=" />
       </div>
     </div>
   );
